e9500479ab4462764ebf3161c8be6a0f0dfcc47d
Dexter Gaon-Shatford <dexter@gaonshatford.ca>
2021-10-11 16:34:42 -0400

    add published stream types

--- src/published_stream.h
+++ src/published_stream.h
@@ -7,7 +7,7 @@
 
 struct published_stream_data {
     SRTSOCKET sock;
-    char * stream_name;
+    const char * name;
 
     pthread_mutex_t num_subscribers_lock;
     unsigned int num_subscribers;
@@ -23,18 +23,25 @@ struct published_stream_data {
 unsigned int get_num_subscribers(struct published_stream_data * data);
 
 // called from outside the thread for a stream
-void add_srt_subscriber(struct published_stream_data * data);
-// called from inside the thread for a stream
-void remove_srt_subscriber_node(struct published_stream_data * data);
+void add_srt_subscriber(struct published_stream_data * data, SRTSOCKET sock);
+// called from inside the thread for a stream if sending data failed
+// its implementation assumes srt_subscribers_lock has been acquired
+void remove_srt_subscriber_node(
+        struct published_stream_data * data,
+        struct srt_subscriber_node * subscriber);
 
 // called from outside the thread for a stream
 void add_webrtc_subscriber(struct published_stream_data * data);
-// called from inside the thread for a stream
-void remove_webrtc_subscriber_node(struct published_stream_data * data);
+// called from inside the thread for a stream if sending data failed
+// its implementation assumes webrtc_subscribers_lock has been acquired
+void remove_webrtc_subscriber_node(
+        struct published_stream_data * data,
+        struct webrtc_subscriber_node * subscriber);
 
 
 
 struct srt_subscriber_node {
+    SRTSOCKET sock;
     struct srt_subscriber_node * next;
     struct srt_subscriber_node * prev;
 };
@@ -49,19 +56,25 @@ struct webrtc_subscriber_node {
 struct published_stream_map;
 
 struct published_stream_map * create_published_stream_map(
-        unsigned int max_publishers, unsigned int max_subscribers_per_publisher);
+        unsigned int max_publishers,
+        unsigned int max_subscribers_per_publisher);
 
 bool max_publishers_exceeded(struct published_stream_map * map);
-bool max_subscribers_exceeded(
-        struct published_stream_map * map, const char * name);
+
 bool stream_name_in_map(struct published_stream_map * map, const char * name);
+
 char ** stream_names(struct published_stream_map * map);
+
 void add_stream_to_map(
-        struct published_stream_map * map, const char * name, SRTSOCKET sock);
+        struct published_stream_map * map, SRTSOCKET sock, const char * name);
+
 void remove_stream_from_map(
         struct published_stream_map * map, const char * name);
+
 struct published_stream_data * get_stream_from_map(
         struct published_stream_map * map, const char * name);
 
+bool max_subscribers_exceeded(
+        struct published_stream_map * map, const char * name);
 
 #endif
