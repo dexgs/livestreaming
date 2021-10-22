586ad20b4793edf25a48e3a6965d871b7431bf19
Dexter Gaon-Shatford <dexter@gaonshatford.ca>
2021-10-22 12:30:20 -0400

    Add test

---
+++ test/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "../src/published_stream.h"
+#include "../src/authenticator.h"
+
+/* These tests are intended to make sure the code I have written is behaving
+ * properly. The tests do not cover the behaviour of third-party code like SRT
+ * or PicoHTTPParser, as their correctness is not my responsibility */
+
+int main() {
+    printf("Running tests!\n");
+}
