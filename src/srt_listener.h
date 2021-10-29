029d76023f523065016b8de5ce3f3282fcf0aaf3
Dexter Gaon-Shatford <dexter@gaonshatford.ca>
2021-10-29 11:03:55 -0400

    Move SRT options into defines

--- src/srt_listener.h
+++ src/srt_listener.h
@@ -8,6 +8,38 @@
 #define SRT_LISTEN_BACKLOG 4
 #endif
 
+#ifndef MIN_PACKETS_IN_FLIGHT
+#define MIN_PACKETS_IN_FLIGHT 1024
+#endif
+
+#ifndef SEND_BUFFER_SIZE
+#define SEND_BUFFER_SIZE 1490944
+#endif
+
+#ifndef RECV_BUFFER_SIZE
+#define RECV_BUFFER_SIZE 1490944
+#endif
+
+#ifndef OVERHEAD_BW_PERCENT
+#define OVERHEAD_BW_PERCENT 10
+#endif
+
+#ifndef LATENCY_MS
+#define LATENCY_MS 0
+#endif
+
+#ifndef ENABLE_TIMESTAMPS
+#define ENABLE_TIMESTAMPS false
+#endif
+
+#ifndef ENABLE_REPEATED_LOSS_REPORTS
+#define ENABLE_REPEATED_LOSS_REPORTS false
+#endif
+
+#ifndef ENABLE_DRIFT_TRACER
+#define ENABLE_DRIFT_TRACER false
+#endif
+
 void start_srt_listeners(
         unsigned short publish_port, unsigned short subscribe_port,
         char * publish_passphrase, char * subscribe_passphrase,
