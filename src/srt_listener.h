bbda4151851b1b7eeda9acbfa90f3a27944a7bd3
chili-b <chili-b@noreply.localhost>
2021-10-29 13:26:19 -0400

    Update 'src/srt_listener.h'

--- src/srt_listener.h
+++ src/srt_listener.h
@@ -29,7 +29,7 @@
 #endif
 
 #ifndef ENABLE_TIMESTAMPS
-#define ENABLE_TIMESTAMPS false
+#define ENABLE_TIMESTAMPS true
 #endif
 
 #ifndef ENABLE_REPEATED_LOSS_REPORTS
@@ -37,7 +37,7 @@
 #endif
 
 #ifndef ENABLE_DRIFT_TRACER
-#define ENABLE_DRIFT_TRACER false
+#define ENABLE_DRIFT_TRACER true
 #endif
 
 void start_srt_listeners(
