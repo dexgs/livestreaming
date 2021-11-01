dbfa73f69d48babe86abdf6acf0e6009e1e7d938
Dexter Gaon-Shatford <dexter@gaonshatford.ca>
2021-11-01 01:07:25 -0400

    Make web send buffer size configurable

--- src/web_listener.h
+++ src/web_listener.h
@@ -9,6 +9,10 @@
 #define WEB_LISTEN_BACKLOG 10
 #endif
 
+#ifndef WEB_SEND_BUFFER_SIZE
+#define WEB_SEND_BUFFER_SIZE 1490944
+#endif
+
 void start_web_listener(
         unsigned short port, bool read_web_ip_from_headers,
         struct authenticator * auth, struct published_stream_map * map);
